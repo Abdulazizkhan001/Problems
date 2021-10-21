ef886374da9e7f8a9ab3f3d6e0ab1cb7a27c0dc4
Abdulazizkhan001 <abdualzizbotirxonov93@gmail.com>
2021-10-21 11:06:12 -0700

    JavaScript problems

---
+++ JavaScript.js
@@ -0,0 +1,56 @@
+//1-problem -- SOLVED
+let a = 5;
+let P = 4 * a;
+console.log(P);
+
+
+//2-problem -- SOLVED
+
+let a = 5;
+let S = a * a;
+console.log(S);
+
+//3-problem -- SOLVED
+
+let a = 5, b = 4;
+let S = a * b;
+let P = 2 * (a + b);
+console.log('To\'rtburchakning yuzasi =',S , 'To\'rtburchakning permetri =',P);
+
+//4-problem -- SOLVED
+
+let p = 3.14;
+let d = 4;
+let L = p * d;
+console.log('Aylananing uzunligi -',L);
+
+
+//5-problem -- SOLVED
+
+let a = 5;
+let V = a * (a * a);
+let S = 6 * ( a * a);
+console.log('Kubning hajmi -', V , 'Kubning to\'la sirti -',S);
+
+//6-problem -- SOLVED
+
+let a = 4, b = 8, c = 12;
+let V = a * b * c;
+let S = 2 * (a * b + b * c + a * c);
+ console.log('Paralelepepidning hajmi -', V , 'Paralelepepidning to\'la sirti -',S);
+
+//7-problem -- SOLVED
+
+let R = 7;
+let L = 2 * 3.14 * R;
+let S = 3.14 *(R * R);
+console.log('Doiraning uzunligi -',L, 'Doiraning yuzasi -',S );
+
+//8-problem -- SOLVED
+
+let a = 15, b = 20;
+let c = (a + b) / 2;
+console.log('O\'rta arifmetigi =',c);
+
+
+
