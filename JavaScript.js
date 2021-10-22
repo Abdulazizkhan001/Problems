dd52790b135c9382e9949a76a6d2e45dba64a233
Abdulazizkhan001 <abdualzizbotirxonov93@gmail.com>
2021-10-21 22:27:44 -0700

    js changes

--- JavaScript.js
+++ JavaScript.js
@@ -1,56 +1,161 @@
-//1-problem -- SOLVED
-let a = 5;
-let P = 4 * a;
-console.log(P);
+// //1-problem -- SOLVED
+// let a = 5;
+// let P = 4 * a;
+// console.log(P);
 
 
-//2-problem -- SOLVED
+// //2-problem -- SOLVED
 
-let a = 5;
-let S = a * a;
-console.log(S);
+// let a = 5;
+// let S = a * a;
+// console.log(S);
 
-//3-problem -- SOLVED
+// //3-problem -- SOLVED
 
-let a = 5, b = 4;
-let S = a * b;
-let P = 2 * (a + b);
-console.log('To\'rtburchakning yuzasi =',S , 'To\'rtburchakning permetri =',P);
+// let a = 5, b = 4;
+// let S = a * b;
+// let P = 2 * (a + b);
+// console.log('To\'rtburchakning yuzasi =',S , 'To\'rtburchakning permetri =',P);
 
-//4-problem -- SOLVED
+// //4-problem -- SOLVED
 
-let p = 3.14;
-let d = 4;
-let L = p * d;
-console.log('Aylananing uzunligi -',L);
+// let p = 3.14;
+// let d = 4;
+// let L = p * d;
+// console.log('Aylananing uzunligi -',L);
 
 
-//5-problem -- SOLVED
+// //5-problem -- SOLVED
 
-let a = 5;
-let V = a * (a * a);
-let S = 6 * ( a * a);
-console.log('Kubning hajmi -', V , 'Kubning to\'la sirti -',S);
+// let a = 5;
+// let V = a * (a * a);
+// let S = 6 * ( a * a);
+// console.log('Kubning hajmi -', V , 'Kubning to\'la sirti -',S);
 
-//6-problem -- SOLVED
+// 6-problem -- SOLVED
 
-let a = 4, b = 8, c = 12;
-let V = a * b * c;
-let S = 2 * (a * b + b * c + a * c);
- console.log('Paralelepepidning hajmi -', V , 'Paralelepepidning to\'la sirti -',S);
+// let a = 4, b = 8, c = 12;
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+//  console.log('Paralelepepidning hajmi -', V , 'Paralelepepidning to\'la sirti -',S);
 
-//7-problem -- SOLVED
+// 7-problem -- SOLVED
 
-let R = 7;
-let L = 2 * 3.14 * R;
-let S = 3.14 *(R * R);
-console.log('Doiraning uzunligi -',L, 'Doiraning yuzasi -',S );
+// let R = 7;
+// let L = 2 * 3.14 * R;
+// let S = 3.14 *(R * R);
+// console.log('Doiraning uzunligi -',L, 'Doiraning yuzasi -',S );
 
-//8-problem -- SOLVED
+// 8-problem -- SOLVED
 
-let a = 15, b = 20;
-let c = (a + b) / 2;
-console.log('O\'rta arifmetigi =',c);
+// let a = 15, b = 20;
+// let c = (a + b) / 2;
+// console.log('O\'rta arifmetigi =',c);
+
+// //9-problem -- SOLVED
+
+// var a = 4;
+// var b = 9;
+// var kub = (a * b) ** (1 / 2);
+// console.log(kub);
+
+// //10-problem -- SOLVED
+
+// var a = 10,
+//   b = 12;
+// var yigindi = a + b;
+// var kopaytma = a * b;
+// var kvadrat = a ** 2;
+// let kvadratb = b ** 2;
+// console.log(yigindi, kopaytma, kvadrat, kvadratb);
+
+// // 11
+
+// //12-problem -- SOLVED
+
+// var a = 12;
+// var b = 13;
+// var c = (a ** 2 + b ** 2) ** (1 / 2);
+// var P = a + b + c;
+// console.log(c, P);
+
+// //13-problem -- SOLVED
+
+// var r1 = 10;
+// var r2 = 5;
+// var s1 = p * r1;
+// var s2 = p * r2;
+// var s3 = p * (r1 - r2);
+// console.log(s1, s2, s3);
+
+// //14-problem -- SOLVED
+// var L = 12.56;
+// var r = L / (2 * p);
+// console.log(r, (s = p * r ** 2));
+
+// //15-problem -- SOLVED
+
+// var S = 15;
+// var R = (S / p) ** (1 / 2);
+// console.log(R);
+
+// //16-problem -- SOLVED
+
+// var x2 = 10;
+// var x1 = 4;
+// console.log(x2 - x1);
+
+// //17-problem -- SOLVED
+// var A = 3;
+// var B = 7;
+// var C = 10;
+// var AC = C - A;
+// var BC = C - B;
+
+// console.log(AC, BC, AC + BC);
+
+// //18-problem -- SOLVED
+
+// var A = 2;
+// var C = 8;
+// var B = 13;
+// var AC = C - A;
+// var BC = B - C;
+// console.log(AC, BC, AC * BC);
+
+// //19-problem -- SOLVED
+
+// var x1 = 8;
+// var y1 = 2;
+// var x2 = 15;
+// var y2 = 6;
+
+// console.log(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2));
+
+// //22-problem -- SOLVED
+
+// var a = 34;
+// var b = 42;
+// var c;
+// var c = a;
+// var a = b;
+// var b = c;
+
+// console.log(a, b);
+
+// //23-problem -- SOLVED
+
+// var a = 34;
+// var b = 42;
+// var c = 53;
+// var d;
+// var d = a;
+// var a = b;
+// var b = c;
+// var c = d;
+// console.log(a, b, c);
+
+
 
 
 
